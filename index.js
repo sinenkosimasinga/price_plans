8e35fe770a4184484dd3757a847bceaa63f098db
Sinenkosi Masinga <SinenkosiM@netstar.co.za>
2022-09-04 17:00:26 +0200

    price plan commit

---
+++ index.js
@@ -0,0 +1,83 @@
+import * as sqlite from 'sqlite';
+import  sqlite3 from 'sqlite3';
+import  express  from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const db = await sqlite.open({
+    filename: './data_plan.db',
+    driver: sqlite3.Database
+});
+
+console.log('db initialized');
+
+await db.migrate();
+
+app.get('/api/price_plans', async function(req,res){
+    const price_plans= await db.all(`select * from price_plan`);
+    res.json({
+        price_plans
+    })
+});
+
+
+app.post('/api/price_plan/update', async function(req,res){
+
+    console.log(req.body)
+
+    const {sms_price, call_price, price_plan} = req.body;
+
+    const result= await db.run(`UPDATE price_plan SET sms_price = ?, call_price = ? where plan_name = ?`,
+    sms_price, call_price, price_plan);
+
+    console.log(result)
+
+    res.json({
+        status : 'success'
+    })
+
+});
+
+app.post('/api/phonebill/', async function(req,res){
+
+    const price_plan_name = req.body.price_plan
+
+    //get prices to use
+    const price_plan= await db.get(`SELECT id, plan_name, sms_price, call_price
+            FROM price_plan where plan_name=?`,price_plan_name);
+
+    if (!price_plan){
+        res.json({
+            error : `Invalid plan name :${price_plan_name}`
+        });
+    }else{
+        const activity =req.body.actions;
+
+        const activities = activity.split(",");
+        let total = 0;
+
+        activities.forEach(action => {
+            if (action.trim() === 'sms') {
+            total += price_plan.sms_price;
+            } else if (action.trim() == 'call') {
+                total += price_plan.call_price;
+            }
+        });
+
+        res.json({
+            total
+        })
+
+    }
+
+    //Use the price plan to calculate the total cost
+    
+});
+console.log('done!');
+
+const PORT = 3001;
+app.listen(PORT, function(){
+    console.log(`Price plan API started on port ${PORT}`)
+});
